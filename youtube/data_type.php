7c8a92625e013f2f2f0ea32e7023e2250504e7ba
こた <tokyosato@gmail.com>
2021-09-07 11:52:40 +0900

    first commit

---
+++ youtube/data_type.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP practice</title>
+</head>
+<body>
+  <h3>
+    <?php
+      // string
+      echo 'こんにちは、' . '文字列型です';
+      echo '<br/>';
+      $name = 'sato kota';
+      echo $name;
+      echo '<br/>';
+      // int
+      $result = 10 * 5 + 4 - 2 / 2;
+      echo $result;
+      echo '<br/>';
+      echo $result + (10 % 3);
+      echo '<br/>';
+      // float
+      echo 10.0 / 3.0;
+      echo '<br/>';
+      // bool
+      echo TRUE || FALSE;
+      echo TRUE or FALSE;
+      echo '<br/>';
+      // falseは出力されない
+      echo TRUE && FALSE;
+      echo TRUE and FALSE;
+    ?>
+</h3>
+</body>
+</html>
