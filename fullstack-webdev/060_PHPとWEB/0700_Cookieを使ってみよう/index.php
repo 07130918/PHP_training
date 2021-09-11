8f410b6b2072b8e31d374336424a7ca2286b2ce2
こた <tokyosato@gmail.com>
2021-09-11 17:42:25 +0900

    cookie session

--- fullstack-webdev/060_PHPとWEB/0700_Cookieを使ってみよう/index.php
+++ fullstack-webdev/060_PHPとWEB/0700_Cookieを使ってみよう/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 setcookie('VISIT_COUNT', 1, [
     'expires' => time() + 60 * 60 * 24 * 30,
     'path' => '/'
--- fullstack-webdev/060_PHPとWEB/0800_Sessionを使ってみよう/index.php
+++ fullstack-webdev/060_PHPとWEB/0800_Sessionを使ってみよう/index.php
@@ -1,5 +1,4 @@
-<?php 
+<?php
 session_start();
 $_SESSION['VISIT_COUNT'] = 1;
 echo $_SESSION['VISIT_COUNT'];
-// phpinfo();
