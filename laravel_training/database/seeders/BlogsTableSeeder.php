9f16ef74b60bf0e884c282e908aba52f0bdf781e
こた <tokyosato@gmail.com>
2021-09-12 17:20:35 +0900

    add dummy data

--- laravel_training/database/seeders/BlogsTableSeeder.php
+++ laravel_training/database/seeders/BlogsTableSeeder.php
@@ -3,16 +3,16 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
-class DatabaseSeeder extends Seeder
+use App\Models\Blog;
+class BlogsTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(BlogsTableSeeder::Class);
+        \App\Models\Blog::factory()->count(15)->create();
     }
 }
