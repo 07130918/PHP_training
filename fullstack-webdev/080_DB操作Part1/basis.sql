3a9b40a7663c0d8dbbb5cbd783f1280227a3d87b
こた <tokyosato@gmail.com>
2021-09-26 21:49:15 +0900

    column add modify drop

--- fullstack-webdev/080_DB操作Part1/basis.sql
+++ fullstack-webdev/080_DB操作Part1/basis.sql
@@ -25,7 +25,7 @@ FOREIGN KEY: 外部キー制約
 -- primaryはnot null かつ uniqueみたいなもの
 -- テーブルの作成
 create table test_db.test_table (
-	id int(6) NOT NULL unsigned default 0 comment 'ID',
+	key1 int auto_increment primary key,
     val varchar(20) UNIQUE default 'hello' comment '値' -- 最後は,で終わらないように
 );
 
@@ -39,3 +39,30 @@ show full columns from test_table;
 show CREATE table test_table ;
 -- アクティブなDBの確認
 SELECT DATABASE();
+
+/*
+テーブル定義の変更
+*/
+alter table test_db.test_table
+add column key2 varchar(20) not null,
+add column key3 varchar(20) not null;
+
+alter table test_db.test_table
+add column key4 varchar(20) after key2;
+
+alter table test_db.test_table
+add column key5 varchar(20) first;
+
+alter table test_db.test_table
+modify column key5 int not null;
+
+alter table test_db.test_table
+modify column key1 int(11) NOT NULL;
+
+alter table test_db.test_table
+drop column key5;
+
+alter table test_db.test_table
+drop primary key;
+
+show create table test_db.test_table;
