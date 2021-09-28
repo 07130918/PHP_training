47f430df1e8e37242af2fc2192f251371f4b3f32
こた <tokyosato@gmail.com>
2021-09-28 11:59:41 +0900

    name fix

--- fullstack-webdev/080_DB操作Part1/主キーの作成/pk.sql
+++ fullstack-webdev/080_DB操作Part1/主キーの作成/pk.sql
@@ -1,8 +1,9 @@
 /*
 主キーの作成
-一つのテーブルにつき一つの主キーのみ作成可能。
-一意の値であること。NOT NULLであること。
+1つのテーブルにつき1つの主キーのみ作成可能。
+1意の値であること。NOT NULLであること。
 */
+
 -- 単一主キー
 create table test_db.test_table (
 	key1 int(6) PRIMARY KEY
--- fullstack-webdev/090_DB操作Part2/where.sql
+++ fullstack-webdev/090_DB操作Part2/where.sql
@@ -2,34 +2,27 @@
 条件句の設定
 */
 -- = : 一致
-select * from test_db.txn_stocks
-where product_id = 1;
+select * from test_db.txn_stocks where product_id = 1;
 
 -- <>, != : 非一致
-select * from test_db.txn_stocks
-where product_id != 1;
+select * from test_db.txn_stocks where product_id != 1;
 
 -- >, >=, <, <= : 数値の比較
-select * from test_db.txn_stocks
-where amount >= 60;
+select * from test_db.txn_stocks where amount >= 60;
 
 -- A and B : A かつ B
-select * from test_db.txn_stocks
-where product_id = 1 and shop_id = 1;
+select * from test_db.txn_stocks where product_id = 1 and shop_id = 1;
 
 -- A or B : A または B
-select * from test_db.txn_stocks
-where product_id = 1 or shop_id = 1;
+select * from test_db.txn_stocks where product_id = 1 or shop_id != 1;
 
 -- () : 条件をくくる
 select * from test_db.txn_stocks
-where (product_id = 1 and shop_id = 1)
-or (product_id = 2 and shop_id = 2);
+where (product_id = 1 and shop_id = 1) or (product_id = 2 and shop_id = 2);
 
 -- like : %で部分一致検索
 -- _とした場合には一文字に一致
-select * from test_db.mst_shops
-where name like '店%';
+select * from test_db.mst_shops where name like '店%A';
 
 -- in : いずれかの値に一致
 select * from test_db.mst_shops
@@ -39,7 +32,7 @@ where name in ('店舗A', '店舗B');
 select * from test_db.mst_shops
 where name not in ('店舗A', '店舗B');
 
--- between A and B: A から B の値 
+-- between A and B: A から B の値
 select * from test_db.txn_stocks
 where amount between 60 and 100;
 
