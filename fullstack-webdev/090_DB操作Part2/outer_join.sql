4a4125e5ac27e01cfbd617a59437b802aee9feab
こた <tokyosato@gmail.com>
2021-09-28 16:33:41 +0900

    outer join

--- fullstack-webdev/090_DB操作Part2/outer_join.sql
+++ fullstack-webdev/090_DB操作Part2/outer_join.sql
@@ -1,2 +1,17 @@
 -- 条件に当てはまる2つのテーブルのすべてのレコードを返す
--- MySQLにこの機能は無い
+-- FULL[OUTER]JOIN MySQLにこの機能は無い
+-- テーブル外部結合 LEFT OUTER JOIN と RIGHT OUTER JOIN
+select * from 左テーブル
+left join 右テーブル
+on 左テーブル.値が一致する属性 = 右テーブル.値が一致する属性;
+
+
+SELECT * from test_db.mst_prefs;
+
+insert into test_db.mst_prefs (name, updated_by) value
+('山形', 'satokota'),
+('沖縄', 'satokota');
+
+SELECT * from test_db.mst_prefs mp
+left join test_db.mst_shops ms
+on ms.pref_id = mp.id;
