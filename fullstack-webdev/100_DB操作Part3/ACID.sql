e1e5e800ba8a77f1676808287a7e0caf2486f993
こた <tokyosato@gmail.com>
2021-09-30 10:07:40 +0900

    acid

--- fullstack-webdev/100_DB操作Part3/ACID.sql
+++ fullstack-webdev/100_DB操作Part3/ACID.sql
@@ -1,19 +1,14 @@
 /*
 【ACID特性】トランザクション
 */
--- セッションA
+
 start transaction;
 
 insert into test_db.txn_stocks(product_id, shop_id, amount, updated_by)
-values (1, 3, 20, 'codemafia');
+values (1, 3, 20, 'kota');
 
 update test_db.txn_stocks set amount = 1000
 where product_id = 1 and shop_id = 1;
-
--- 同一セッション内の確認
-select * from test_db.txn_stocks
-where product_id = 1 and shop_id = 3;
-
 
 -- 成功
 commit;
