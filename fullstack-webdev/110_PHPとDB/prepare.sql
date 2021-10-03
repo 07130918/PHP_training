0b739d5bc354157c0b7d36c101e65e00ee84c9ae
こた <tokyosato@gmail.com>
2021-10-03 13:51:57 +0900

    prepared

--- fullstack-webdev/110_PHPとDB/prepare.sql
+++ fullstack-webdev/110_PHPとDB/prepare.sql
@@ -1,16 +1,10 @@
 /*
 # 以下を設定すると実行されたクエリがログに出力されます。
+mysqlに再度接続する場合その都度やる
+
 set global general_log = 'on';
-# Windows
-set global general_log_file = 'C:/MAMP/logs/mysql_query.log'; -- Windows
-
-# Mac, Linux
-set global general_log_file = '/Applications/MAMP/logs/mysql_query.log'; -- Mac, Linux
-
+set global general_log_file = '/Applications/MAMP/logs/mysql_query.log';
 # ログの確認
-- Windows
-Get-Content C:\MAMP\logs\mysql_query.log -Wait -Tail 20
-- Mac, Linux
 tail -f /Applications/MAMP/logs/mysql_query.log
 */
 
@@ -33,15 +27,11 @@ GRANT ALL ON test_phpdb.* TO 'test_user'@'localhost';
 
 START TRANSACTION;
 
-SET @editor = 'codemafia';
+SET @editor = 'kota';
 
 SET time_zone = "+09:00";
 
 -- --------------------------------------------------------
-
---
--- Table structure for table mst_prefs
---
 
 CREATE TABLE mst_prefs (
   id int(2) UNSIGNED NOT NULL,
@@ -50,10 +40,6 @@ CREATE TABLE mst_prefs (
   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   updated_by varchar(20) NOT NULL
 );
-
---
--- Dumping data for table mst_prefs
---
 
 INSERT INTO mst_prefs (id, name, delete_flg, updated_by) VALUES
 (1, '北海道', 0, @editor),
@@ -64,10 +50,6 @@ INSERT INTO mst_prefs (id, name, delete_flg, updated_by) VALUES
 
 -- --------------------------------------------------------
 
---
--- Table structure for table mst_products
---
-
 CREATE TABLE mst_products (
   id int(10) UNSIGNED NOT NULL,
   name varchar(20) NOT NULL,
@@ -76,20 +58,12 @@ CREATE TABLE mst_products (
   updated_by varchar(20) NOT NULL
 );
 
---
--- Dumping data for table mst_products
---
-
 INSERT INTO mst_products (id, name, delete_flg, updated_by) VALUES
 (1, 'テーブル', 0, @editor),
 (2, '椅子', 1, @editor),
 (3, 'ベッド', 0, @editor);
 
 -- --------------------------------------------------------
-
---
--- Table structure for table mst_shops
---
 
 CREATE TABLE mst_shops (
   id int(10) UNSIGNED NOT NULL,
@@ -100,20 +74,12 @@ CREATE TABLE mst_shops (
   updated_by varchar(20) NOT NULL
 );
 
---
--- Dumping data for table mst_shops
---
-
 INSERT INTO mst_shops (id, name, pref_id, delete_flg, updated_by) VALUES
 (1, '店舗A', 1, 0, @editor),
 (2, '店舗B', 2, 0, @editor),
 (3, '店舗C', 3, 0, @editor);
 
 -- --------------------------------------------------------
-
---
--- Table structure for table txn_stocks
---
 
 CREATE TABLE txn_stocks (
   product_id int(10) UNSIGNED NOT NULL,
@@ -123,7 +89,6 @@ CREATE TABLE txn_stocks (
   updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   updated_by varchar(20) NOT NULL
 );
-
 
 INSERT INTO txn_stocks (product_id, shop_id, amount, delete_flg, updated_by) VALUES
 (1, 1, 10, 0, @editor),
@@ -138,71 +103,38 @@ INSERT INTO txn_stocks (product_id, shop_id, amount, delete_flg, updated_by) VAL
 
 --
 -- Indexes for dumped tables
---
 
---
--- Indexes for table mst_prefs
---
 ALTER TABLE mst_prefs
   ADD PRIMARY KEY (id);
 
---
--- Indexes for table mst_products
---
 ALTER TABLE mst_products
   ADD PRIMARY KEY (id);
 
---
--- Indexes for table mst_shops
---
 ALTER TABLE mst_shops
   ADD PRIMARY KEY (id),
   ADD KEY fk_pref_id (pref_id);
 
---
--- Indexes for table txn_stocks
---
 ALTER TABLE txn_stocks
   ADD PRIMARY KEY (product_id,shop_id),
   ADD KEY fk_shop_id (shop_id);
 
---
 -- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table mst_prefs
---
 ALTER TABLE mst_prefs
   MODIFY id int(2) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
---
--- AUTO_INCREMENT for table mst_products
---
 ALTER TABLE mst_products
   MODIFY id int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT for table mst_shops
---
 ALTER TABLE mst_shops
   MODIFY id int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
 -- Constraints for dumped tables
---
 
---
--- Constraints for table mst_shops
---
 ALTER TABLE mst_shops
   ADD CONSTRAINT fk_pref_id FOREIGN KEY (pref_id) REFERENCES mst_prefs (id) ON UPDATE CASCADE;
 
---
--- Constraints for table txn_stocks
---
 ALTER TABLE txn_stocks
   ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES mst_products (id) ON UPDATE CASCADE,
   ADD CONSTRAINT fk_shop_id FOREIGN KEY (shop_id) REFERENCES mst_shops (id) ON UPDATE CASCADE;
 COMMIT;
-
