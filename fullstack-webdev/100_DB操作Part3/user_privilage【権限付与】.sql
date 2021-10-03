eb0b43eedbd0a78273f3482c7d9f2803d623e571
こた <tokyosato@gmail.com>
2021-10-03 10:00:47 +0900

    create user

--- fullstack-webdev/100_DB操作Part3/user_privilage【権限付与】.sql
+++ fullstack-webdev/100_DB操作Part3/user_privilage【権限付与】.sql
@@ -5,7 +5,7 @@
 show grants for test_user@localhost;
 
 -- 権限の付与
-grant {権限名} on {対象のDBオブジェクト} to {ユーザー};
+-- grant {権限名} on {対象のDBオブジェクト} to {ユーザー};
 grant all on test_db.* to 'test_user'@'localhost';
 grant update, insert on test_db.* to 'test_user'@'localhost';
 grant create, alter on test_db.* to 'test_user'@'localhost';
