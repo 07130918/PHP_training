1b062c6ee76ed88a1aa4b3eff785ac7d16a5ed5b
こた <tokyosato1@gmail.com>
2021-11-14 16:30:34 +0900

    dbから値の取得

--- fullstack-webdev/110_PHPとDB/0100_DBから値を取得/index.php
+++ fullstack-webdev/110_PHPとDB/0100_DBから値を取得/index.php
@@ -1,20 +1,16 @@
 <?php
-/**
- * DBから値を取得
- */
 $user = 'test_user';
 $pwd = 'pwd';
-$host = 'localhost';
+$host= 'localhost';
 $dbName = 'test_phpdb';
-$dsn = "mysql:host={$host};port=8889;dbname={$dbName};";
-$conn = new PDO($dsn, $user, $pwd);
+$dns = "mysql:host={$host};port=8889;dbname={$dbName}";
+$conn = new PDO($dns, $user, $pwd);
 $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$pst = $conn->query('select * from mst_shops');
+$pst = $conn->query('SELECT * FROM mst_shops');
 $result = $pst->fetchAll();
 
 echo '<pre>';
 print_r($result);
 echo '</pre>';
-
 $conn = null;
