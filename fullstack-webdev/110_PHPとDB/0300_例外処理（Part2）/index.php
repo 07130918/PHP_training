daa4dbb2abe02d5ace71a9ecf88c16b5ea8312ae
こた <tokyosato1@gmail.com>
2021-11-14 17:54:27 +0900

    part2

--- fullstack-webdev/110_PHPとDB/0300_例外処理（Part2）/index.php
+++ fullstack-webdev/110_PHPとDB/0300_例外処理（Part2）/index.php
@@ -2,42 +2,25 @@
 /**
  * 例外処理
  */
-
 function throwException() {
     try {
-
+        // new PDO('', '', '');
         $bool = false;
-        // $bool->method();
-        new PDO('', '', '');
-    
+        $bool->method();
         echo '通常処理が最後まで実行されました。<br>';
-    
-    } catch(Throwable $e) {
-    
+    } catch (PDOException $e) {
         echo 'PDOException<br>';
         echo '例外処理が実行されました。<br>';
         echo $e->getMessage() . '<br>';
-        echo get_class($e) . '<br>';
-    
     }
 }
 
 try {
-
     throwException();
-    
     echo '通常処理が最後まで実行されました。<br>';
-
-} catch(Error $e) {
-
+} catch (Error $e) {
     echo '例外処理が実行されました。<br>';
     echo $e->getMessage() . '<br>';
-    echo get_class($e) . '<br>';
-
 } finally {
-
     echo '終了処理が実行されました。<br>';
-
 }
-
-echo 'finallyの後です。';
