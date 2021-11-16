491e9815980de2c07281a28f0e6c8df2cb214036
こた <tokyosato1@gmail.com>
2021-11-16 15:34:44 +0900

    transaction

--- fullstack-webdev/110_PHPとDB/0700_トランザクション/index.php
+++ fullstack-webdev/110_PHPとDB/0700_トランザクション/index.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * トランザクション
- * 
  * 例）店舗Aから店舗Cへ商品を転送する場合のトランザクション
+ * データの不整合が起こらないために必要(例えば合計在庫数が合うように)
  */
 try {
-
+    
     $user = 'test_user';
     $pwd = 'pwd';
     $host = 'localhost';
@@ -16,19 +16,19 @@ try {
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-    $product_id = 2;
+    $product_id = 3;
     $from_shop_id = 1;
     $to_shop_id = 3;
     $amount = 10;
 
-    $pst = $conn->prepare('
-        update txn_stocks
-        set amount = amount + :amount
-        where shop_id = :shop_id
-        and product_id = :product_id
-    ');
-
     $conn->beginTransaction();
+
+    $pst = $conn->prepare('
+        UPDATE txn_stocks
+        SET amount = amount + :amount
+        WHERE shop_id = :shop_id
+        AND product_id = :product_id
+    ');
 
     $pst->execute([
         ':amount' => $amount,
@@ -43,10 +43,12 @@ try {
     ]);
 
     $conn->commit();
+    echo 'コミット完了<br>';
 
 } catch (PDOException $e) {
 
     echo 'エラーが発生しました。<br>';
     echo $e->getMessage();
     $conn->rollBack();
+
 }
